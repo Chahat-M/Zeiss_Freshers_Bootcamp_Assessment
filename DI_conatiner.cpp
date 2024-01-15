8b745e0b5632a20af0604286683446cf37d5de89
Chahat Mittal <76607503+Chahat-M@users.noreply.github.com>
2024-01-15 23:01:38 +0530

    Update DI_conatiner.cpp
    Included DI container implementation via boost library

--- DI_conatiner.cpp
+++ DI_conatiner.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
+#include <boost/di.hpp>
 
 using namespace std;
 
-class FuelPump{
+class IFuelPump{
 public:
     virtual void pumpFuel() = 0;
 };
 
-class StartUpMotor{
+class FuelPump : public IFuelPump{
+public:
+    void pumpfuel() override{
+        // some code
+    }
+}
+
+class IStartUpMotor{
 public:
     virtual void start() = 0;
 };
 
-class Engine{
+class StartUpMotor : public IStartUpMotor{
+    void start() override{
+        // some code
+    }
+}
+
+class IEngine {
+public:
+    virtual void startEngine() = 0;
+};
+
+class Engine : public IEngine{
 private:
-    StartUpMotor* startUpMotorobj;
-    FuelPump* fuelPumpobj;
+    IStartUpMotor* startUpMotorobj;
+    IFuelPump* fuelPumpobj;
 
 public:
-    Engine(StartUpMotor* motor, FuelPump* pump) : startUpMotorobj(motor), fuelPumpobj(pump) {}
+    Engine(IStartUpMotor* motor, IFuelPump* pump) : startUpMotorobj(motor), fuelPumpobj(pump) {}
 
     void startEngine() {
         startUpMotorobj->start();
@@ -27,30 +46,25 @@ public:
 
 };
 
-class Transmission{
+class ITransmission{
 public:
     virtual void shift() = 0;
 }
 
-class CarEngine : public Engine {
+class Transmission : public ITransmission{
 public:
-    CarEngine(StartUpMotor* motor, FuelPump* pump) : Engine(motor, pump) {}
+    void shift() override{
+        // some code
+    }
+}
 
-    void drive() {}
-};
-
-class AutomaticTransmission : public Transmission {
-public:
-    void shift() override {}
-};
-
-class Car {
+class Car : public IEngine, public ITransmission {
 private:
-    CarEngine* engine;
-    Transmission* transmission;
+    IEngine* engine;
+    ITransmission* transmission;
 
 public:
-    Car(CarEngine* carEngine, Transmission* carTransmission)
+    Car(IEngine* carEngine, ITransmission* carTransmission)
         : engine(carEngine), transmission(carTransmission) {}
 
     void startCar() {
@@ -62,3 +76,21 @@ public:
         transmission->shift();
     }
 };
+
+int main()
+{   
+    auto injector = boost::di::make_injector(
+        boost::di::bind<IStartUpMotor>().to<StartUpMotor>(),
+        boost::di::bind<IFuelPump>().to<FuelPump>(),
+        boost::di::bind<IEngine>().to<Engine>(),
+        boost::di::bind<ITransmission>().to<Transmission>()
+    );
+    
+    auto carEngine = injector.create<IEngine>();
+    auto trans = injector.create<ITransmission>();
+
+    Car myCar(*carEngine, *trans);
+    myCar.startEngine();
+    myCar.drive();
+    return 0;
+}
